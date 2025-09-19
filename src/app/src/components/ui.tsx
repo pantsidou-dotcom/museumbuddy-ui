9892f6d1ab3cdf794b738ecef0876afe22092d1d
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-19 20:38:22 +0200

    Update ui.tsx

--- src/app/src/components/ui.tsx
+++ src/app/src/components/ui.tsx
@@ -1,22 +1,27 @@
 'use client';
 import styled from 'styled-components';
+
 export const PageWrapper = styled.div``;
 export const Topbar = styled.header``;
 export const TopbarInner = styled.div``;
 export const Logo = styled.div``;
 export const IconRow = styled.div``;
 export const IconBtn = styled.button``;
+
 export const SearchRow = styled.div``;
 export const SearchInput = styled.input``;
 export const FilterBtn = styled.button``;
+
 export const PageHeader = styled.div``;
 export const SectionHeader = styled.div``;
 export const ChipIcon = styled.button``;
+
 export const ExpoCard = styled.article``;
 export const Fab = styled.button``;
 export const ExpoMeta = styled.div``;
 export const ExpoTitle = styled.h3``;
 export const CTA = styled.button``;
+
 export const VisitorCard = styled.section``;
 export const VisitorTitle = styled.h3``;
 export const ButtonRow = styled.div``;
